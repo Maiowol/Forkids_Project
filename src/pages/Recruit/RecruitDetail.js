6afc3e63de324889dc98531184e8bab60c44fd04
inseong <twin2688@naver.com>
2022-07-09 20:25:58 +0900

    최신

--- src/pages/Recruit/RecruitDetail.js
+++ src/pages/Recruit/RecruitDetail.js
@@ -66,7 +66,6 @@ const RecruitDetail = () => {
 
     // 1:1 문의하기 버튼 눌렀을때 채팅방 생성 + 채팅방 입장하기
     const GoChat = () => {
-
         axios.post('http://13.125.241.180/api/chats/rooms/' + recruitPostId ,null,{
         headers: { Authorization: `Bearer ${getCookie("accessToken")}`}
         })
